138bece93451df88e23e4c6e7d5e40af54c0faea
A-Ra9a <armm8678@gmail.com>
2025-06-27 18:36:59 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+
 import './output.css'; 
+import './index.css';  
+import './App.css';  
 
 
 import App from './App.jsx'
